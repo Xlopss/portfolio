35118d269328ba171c01c4887ed5f8113b9733ee
Xlopss <estelle.alleau@proton.me>
2025-05-09 21:23:54 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+
+    return (
+        <>
+            <div>
+                <h1 className="text-ctp-lavender-700">Estelle Alleau</h1>
+            </div>
+        </>
+    )
+}
