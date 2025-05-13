0924fa6188eb4c239605dd997679e4684980a3e1
Xlopss <estelle.alleau@gmail.com>
2025-05-13 18:28:52 +0200

    Clean + add react-router-dom

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar.tsx";
+
+export default function App() {
+
+    return (
+        <>
+            <Navbar/>
+            <div>
+                <h1 className="text-ctp-lavender-700 text-center">Estelle Alleau</h1>
+            </div>
+        </>
+    )
+}
