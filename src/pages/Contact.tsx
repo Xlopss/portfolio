9c6b87ef1adcc4e69c3a7ce855ce5326a60dc0f4
Xlopss <estelle.alleau@gmail.com>
2025-05-17 17:13:56 +0200

    fix padding in Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,10 +4,10 @@ import ButtonMailTo from "../components/ButtonMailTo.tsx";
 export default function Contact() {
     return (
         <div className="text-ctp-lavender-700 text-center">
-            <div className="p-30 text-3xl">
+            <div className="py-30 text-3xl">
                 <Type/>
             </div>
-            <div className="p-10 px-40">
+            <div className="p-10 ">
                 <p>Si vous avez envie de me contacter pour une alternance, un poste, un projet, une question ou simplement pour discuter,
                     n'hésitez pas à m'écrire.</p>
                 <br/>
