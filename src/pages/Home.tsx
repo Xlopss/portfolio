a1cda2eb0f1895a5b93d89e478efd575e56e7845
Xlopss <estelle.alleau@gmail.com>
2025-06-06 21:47:01 +0200

    Add underscore + add DecryptedText for each title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Skills from "../components/Skills.tsx";
+import DecryptedText from "../components/DecryptedText.tsx";
 
 export default function Home() {
 
@@ -6,9 +7,9 @@ export default function Home() {
 
     return (
         <div className="text-ctp-text text-center flex flex-col items-center">
-            <h1 className="text-ctp-lavender-700 p-10 mb-5">
-                Bienvenue !
-            </h1>
+            <div className="text-ctp-lavender-700 p-10 mb-5 text-5xl">
+                <DecryptedText text="Bienvenue !" speed={80} maxIterations={15} animateOn="view" revealDirection="start" sequential={true} />
+            </div>
             <div className="bg-ctp-mantle shadow-lg shadow-ctp-crust rounded-lg p-6 mb-8">
                 <h2 className="text-xl md:text-2xl mb-5">Salut, moi c'est Estelle 👋</h2>
                 <p className="max-w-2xl w-full mb-3">Je suis une développeuse full-stack sur Lyon qui adore s'investir
@@ -16,7 +17,8 @@ export default function Home() {
                     constante évolution.</p>
                 <p>Je suis actuellement en préparation au titre de Conceptrice et Développeuse d'Applications.</p>
                 <p className="text-ctp-lavender-700">Je suis disponible de suite pour une alternance (12-15 mois) sur
-                    Lyon.</p>
+                    Lyon
+                <span className="underscore">_</span></p>
             </div>
             <Skills/>
             <button onClick={() => window.open(cv, "_blank")}
